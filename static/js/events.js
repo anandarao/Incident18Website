e7858640daeb889d0ff65f439e997fd29b5e3f0f
anandarao <anandarao.h8@gmail.com>
2017-12-18 21:28:00 +0530

    Fixed map zoom and pan in events.html

--- static/js/events.js
+++ static/js/events.js
@@ -1,6 +1,6 @@
 $(function() {
 	$(".button-collapse").sideNav();
-	$(".ribbon-wrapper > img, .ribbon-wrapper > a").animate({opacity:1}), 1000;
+	$(".ribbon-wrapper > img, .ribbon-wrapper > a").animate({opacity:1}, 1000);
 });
 
 function initMap() {
