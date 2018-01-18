c5d1602c25f763f2701262074444ef5ad1206ea5
anandarao <anandarao.h8@gmail.com>
2018-01-18 22:37:30 +0530

    Added Team JSON

--- php/js/team.js
+++ php/js/team.js
@@ -1,4 +1,52 @@
 $(function() {
 	$(".button-collapse").sideNav();
 	$(".ribbon-wrapper > img, .ribbon-wrapper > a").animate({opacity:1}, 1000);
+
+	var content = ""; 
+
+	$.each(team, function(index, object) {
+		var team_members = "";
+		$.each(object, function(index, object) {
+			team_members += `
+				<div class="team-member center">
+					<img src="images/team/${index}.png" class="circle team-image"/>
+					<h4 class="team-name">${this.name}</h4>
+					<p class="team-desig">${this.designation}</p>						
+					<p class="team-details-para">
+						<a href="mailto:${this.email}">${this.email}</a><br/>
+						<a href="tel:+91 ${this.phone}">+91 ${this.phone}</a>
+					</p>
+					<a target="_blank" href="${this.facebook}">
+						<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
+							<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
+						</svg>
+					</a>
+					<a target="_blank" href="${this.linkedin}" style="margin-left: 15px;">
+						<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
+							<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
+						</svg>
+					</a>
+				</div>
+			`;
+		});
+
+		var team_content = `
+			<div class="row valign-wrapper teams-container-row">
+				<div class="col s12 m3 l3">
+					<h1 class="team-heading-name center">${index}</h1>
+				</div>
+				<div class="col s12 m9 l9 members-container"> 
+					${team_members}
+				</div>
+			</div>
+		`;
+
+		content += team_content;
+	});
+
+	$('#teams-container').html(content);
+
+	$(".team-image").on("error", function(){
+        $(this).attr('src', 'http://via.placeholder.com/200x200');
+    });
 });
--- php/team.php
+++ php/team.php
@@ -38,7 +38,7 @@
 		}
 
 	</script>
-
+	<script type="text/javascript" src="js/team.json"></script>
 	<script type="text/javascript" src="js/team.js"></script>
 
 </head>
@@ -77,29 +77,35 @@
 					<h3>Incident Convener</h3>
 					<img src="http://via.placeholder.com/250x250" class="circle" />
 					<h4 class="team-name">Adarsh Pai</h4>
-					<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 96112 13769</p>
-					<a target="_blank" href="#!">
-						<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-							<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-						</svg>
-					</a>
-					<a target="_blank" href="#!" style="margin-left: 15px;">
-						<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-							<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-						</svg>
-					</a>	
+					<p class="team-details-para">
+						<a href="mailto:adarsh@incident.co.in">adarsh@incident.co.in</a><br/>
+						<a href="tel:+91 9611213769">+91 9611213769</a>
+					</p>
+						<a target="_blank" href="https://www.facebook.com/adarsh.pai.3">
+							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
+								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
+							</svg>
+						</a>
+						<a target="_blank" href="https://www.linkedin.com/in/adarshpai/" style="margin-left: 15px;">
+							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
+								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
+							</svg>
+						</a>	
 				</div>
 				<div class="col l6 m6 s12 center add-icons">
 					<h3>Marketing Head</h3>
-					<img src="images/Sabari.png" class="circle" height="250" />
+					<img src="images/team/sabari.png" class="circle" height="250" />
 					<h4 class="team-name">Sabari Ram</h4>
-					<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 96112 13769</p>
-					<a target="_blank" href="#!">
+					<p class="team-details-para">
+						<a href="mailto:sabariram@incident.co.in">sabariram@incident.co.in</a><br/>
+						<a href="tel:+91 7022209252">+91 7022209252</a>
+					</p>
+					<a target="_blank" href="https://www.facebook.com/s.sabariram">
 						<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
 							<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
 						</svg>
 					</a>
-					<a target="_blank" href="#!" style="margin-left: 15px;">
+					<a target="_blank" href="https://www.linkedin.com/in/sabariram96/" style="margin-left: 15px;">
 						<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
 							<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
 						</svg>
@@ -107,137 +113,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="teams-container">
-			<div class="row valign-wrapper teams-container-row">
-				<div class="col s12 m3 l3"><h1 class="team-heading-name center">Web Dev</h1></div>
-				<div class="col s12 m9 l9 members-container">
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-				</div>
-			</div>
-			<div class="row valign-wrapper teams-container-row">
-				<div class="col s12 m3 l3"><h1 class="team-heading-name center">Marketing</h1></div>
-				<div class="col s12 m9 l9 members-container">
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-					<div class="team-member center">
-						<img src="http://via.placeholder.com/200x200" class="circle" />
-						<h4 class="team-name">Name 1</h4>
-						<p class="team-details-para"><a href="mailto:incidentmarketing@nitk.edu.in">incidentmarketing@nitk.edu.in</a><br/>+91 12345 65432</p>
-						<a target="_blank" href="#!">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#4267B2" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z"></path>
-							</svg>
-						</a>
-						<a target="_blank" href="#!" style="margin-left: 15px;">
-							<svg style="width: 30px; height: 30px;" viewBox="0 0 24 24">
-								<path fill="#0077B5" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path>
-							</svg>
-						</a>
-					</div>
-				</div>
-			</div>
+		<div id="teams-container" class="teams-container">
 		</div>
 	</main>
 
